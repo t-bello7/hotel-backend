b6c30511b1e039d1a2c6ce51e27846416e10e0dd
Stephen <stephennnabuike@gmail.com>
2023-02-01 16:18:41 +0100

    Update the swagger file

--- spec/integration/api/v1/users_spec.rb
+++ spec/integration/api/v1/users_spec.rb
@@ -61,32 +61,7 @@ describe 'Users API' do
         run_test!
       end
     end
-  end
-
-  path '/auth/login' do
-    post 'Login with your created email and password' do
-      tags 'Authentication'
-      consumes 'application/json'
-      parameter name: :login, in: :body, schema: {
-        type: :object,
-        properties: {
-          username: { type: :string },
-          email: { type: :string }
-        },
-        required: %w[username password]
-      }
-
-      response '201', '{ token: eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJleHAiOjE2NzUyNjk3MzJ9.v--VeLHaZ3GYPIE8W2oeImOqF4Gz4ozSwxnn4Q9aPpk }' do
-        let(:login) { { email: 'www.example.com', password: 1234 } }
-        run_test!
-      end
-
-      response '422', 'invalid username or password' do
-        let(:auth) { { email: 'example.com' } }
-        run_test!
-      end
-    end
-  end
+  end 
 
   path '/api/v1/users/{id}' do
     get 'Retrieves a user' do
